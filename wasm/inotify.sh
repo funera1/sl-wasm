2d7fdd24e20449ee825ee57c694cd5dd0f34cd87
funera1 <60760935+funera1@users.noreply.github.com>
2024-06-24 08:04:44 +0900

    Add inotify.sh

---
+++ wasm/inotify.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 監視するディレクトリを指定
+WATCH_FILE=$(pwd)/sent-img.zip
+echo Watching $WATCH_FILE...
+
+if [ ! -e $WATCH_FILE ]; then
+  touch $WATCH_FILE
+fi
+
+inotifywait -m $WATCH_FILE
+
+# inotifywaitでディレクトリを監視
+inotifywait -m -e modify,create "$WATCH_FILE" | while read path action file; do
+    # zipを解答
+    unzip $WATCH_FILE img
+    if [ ! -$? -eq 0 ]; then
+      echo $WATCH_FILE is not zip file
+    fi
+
+    mv img/* .
+    rmdir img sent-img.zip
+
+    ./run.sh --restore
+done
