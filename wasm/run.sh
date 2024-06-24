1d04fbf40943d3c5ebe540dea664f6e0225acb3d
funera1 <60760935+funera1@users.noreply.github.com>
2024-06-24 09:04:22 +0900

    オプションをもとに条件分岐

--- wasm/run.sh
+++ wasm/run.sh
@@ -3,17 +3,17 @@ WASMEDGE_DIR=$(pwd)/../funera1-WasmEdge/build
 WASMEDGE_LIB=$WASMEDGE_DIR/lib/api
 WASMEDGE_INCLUDE=$WASMEDGE_DIR/include/api
 
-local_mode=false
-restore_mode=false
+local_mode=0
+restore_mode=0
 
 while (( $# > 0 ))
 do
   case $1 in
     -r)
-      restore_mode=true
+      restore_mode=1
       ;;
     -l)
-      local_mode=true
+      local_mode=1
       ;;
     -h)
       echo "-l: local exec"
@@ -53,13 +53,13 @@ fi
 
 export LD_LIBRARY_PATH=$WASMEDGE_LIB:$LD_LIBRARY_PATH
 clear
-if "${restore_mode}"; then
+if [ $restore_mode -eq 1 ]; then
   ./run-sl-wasm --restore
 else
   ./run-sl-wasm
 fi
 
-
-if "${local_mode}" && [ $? -eq 1 ]; then
+if [ $? -eq 1 ] && [ $local_mode -eq 0 ]; then
+  echo "Sending.."
   ./send.sh
 fi
