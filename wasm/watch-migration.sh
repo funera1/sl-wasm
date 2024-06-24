a601a69d8d16f0e788eb9794d6d6cd0eadfe1905
funera1 <60760935+funera1@users.noreply.github.com>
2024-06-24 09:40:15 +0900

    Rename. For kill this process in exectuion

--- wasm/watch-migration.sh
+++ wasm/watch-migration.sh
@@ -4,6 +4,11 @@
 WATCH_FILE=$(pwd)/sent-img.zip
 echo Watching $WATCH_FILE...
 
+pgrep inotify.sh
+if [ ! $? -eq 0 ]; then
+  kill -9 $(pgrep inotify.sh)
+fi
+
 if [ ! -e $WATCH_FILE ]; then
   touch $WATCH_FILE
 fi
@@ -16,9 +21,6 @@ while : ; do
       if [ ! -$? -eq 0 ]; then
         echo $WATCH_FILE is not zip file
       fi
-
-      mv img/* .
-      rmdir img sent-img.zip
 
       ./run.sh -r
   done
