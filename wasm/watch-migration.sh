6a4c2f885fdf73d018365ca34ace8c2e05c85674
funera1 <60760935+funera1@users.noreply.github.com>
2024-06-27 01:22:16 +0900

    DEBUG: restoreのバグを修正

--- wasm/watch-migration.sh
+++ wasm/watch-migration.sh
@@ -13,17 +13,21 @@ if [ ! -e $WATCH_FILE ]; then
   touch $WATCH_FILE
 fi
 
+function restore () {
+  # zipを解答
+  unzip $WATCH_FILE
+  if [ ! -$? -eq 0 ]; then
+    echo $WATCH_FILE is not zip file
+  fi
+
+  cp img/* .
+  ./run.sh -r -l
+  rm -rf img
+}
+
 # inotifywaitでディレクトリを監視
 while : ; do
   inotifywait -e modify,create "$WATCH_FILE" | while read path action file; do
-      # zipを解答
-      unzip $WATCH_FILE
-      if [ ! -$? -eq 0 ]; then
-        echo $WATCH_FILE is not zip file
-      fi
-
-      cp img/* .
-      rm ./sent-img.zip
-      ./run.sh -r -l
+    restore
   done
 done
