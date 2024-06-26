00327599749eaf1587eaedfec130dc1d89985462
funera1 <60760935+funera1@users.noreply.github.com>
2024-06-27 00:27:00 +0900

    Update watch

--- wasm/watch-migration.sh
+++ wasm/watch-migration.sh
@@ -4,10 +4,10 @@
 WATCH_FILE=$(pwd)/sent-img.zip
 echo Watching $WATCH_FILE...
 
-pgrep inotify.sh
-if [ ! $? -eq 0 ]; then
-  kill -9 $(pgrep inotify.sh)
-fi
+# pgrep watch-migration.sh
+# if [ ! $? -eq 0 ]; then
+#   kill -9 $(pgrep inotify.sh)
+# fi
 
 if [ ! -e $WATCH_FILE ]; then
   touch $WATCH_FILE
@@ -17,11 +17,13 @@ fi
 while : ; do
   inotifywait -e modify,create "$WATCH_FILE" | while read path action file; do
       # zipを解答
-      unzip $WATCH_FILE img
+      unzip $WATCH_FILE
       if [ ! -$? -eq 0 ]; then
         echo $WATCH_FILE is not zip file
       fi
 
+      cp img/* .
+      rm ./sent-img.zip
       ./run.sh -r
   done
 done
