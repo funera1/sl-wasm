7a958a2efc2e1a18235e2c10a013f85be091af74
funera1 <60760935+funera1@users.noreply.github.com>
2024-06-27 13:09:40 +0900

    ISAを判断してビルドを変更

--- wasm/build.sh
+++ wasm/build.sh
@@ -2,13 +2,21 @@ WASMEDGE_DIR=$(pwd)/../funera1-WasmEdge/build
 WASMEDGE_LIB=$WASMEDGE_DIR/lib/api
 WASMEDGE_INCLUDE=$WASMEDGE_DIR/include/api
 
-local_mode=0
-restore_mode=0
+arch=$(uname -m)
+
+if [ "$arch" == "x86_64" ]; then
+  is_x64=1
+  echo "This is a x86_64 system."
+else
+  is_x64=0
+    echo "Unknown architecture: $arch"
+fi
 
 # export C_INCLUDE_PATH=$WASMEDGE_INCLUDE:$C_INCLUDE_PATH
 export CPATH=$WASMEDGE_INCLUDE
 gcc main.c \
   -g                  \
+  -DX64=$is_x64       \
   -o run-sl-wasm      \
   -I$WASMEDGE_INCLUDE \
   -L$WASMEDGE_LIB     \
